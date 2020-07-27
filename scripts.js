17092be22b06504a9df4f301d240a8e85f182ac9
moshiurshimul <hello@moshiurshimul.com>
2020-07-27 06:35:14 +0600

    Final

--- scripts.js
+++ scripts.js
@@ -1,3 +1,21 @@
+// Removing Cart item
+let removeItemBtn = document.getElementsByClassName("remove-item");
+
+for (let i = 0; i < removeItemBtn.length; i++) {
+    const element = removeItemBtn[i];
+
+    element.addEventListener('click', function() {
+        let removeCartItem = document.getElementsByClassName("cart-item-product")
+
+        for (let i = 0; i < removeCartItem.length; i++) {
+            const element = removeCartItem[i];
+            
+            element.remove();
+        }
+    })
+    
+};
+
 //Phone Price
 let phonePrice = document.getElementById('phone-price');
 let phonePriceValue = parseFloat(phonePrice.innerText);
