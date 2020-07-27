e55d1262f329a34fe5ac5d9b96d6a0a952979d3c
moshiurshimul <hello@moshiurshimul.com>
2020-07-27 06:30:55 +0600

    Subtotal and GrandTotal added

--- scripts.js
+++ scripts.js
@@ -1,21 +1,3 @@
-// Removing Cart item
-let removeItemBtn = document.getElementsByClassName("remove-item");
-
-for (let i = 0; i < removeItemBtn.length; i++) {
-    const element = removeItemBtn[i];
-
-    element.addEventListener('click', function() {
-        let removeCartItem = document.getElementsByClassName("cart-item-product")
-
-        for (let i = 0; i < removeCartItem.length; i++) {
-            const element = removeCartItem[i];
-            
-            element.remove();
-        }
-    })
-    
-};
-
 //Phone Price
 let phonePrice = document.getElementById('phone-price');
 let phonePriceValue = parseFloat(phonePrice.innerText);
@@ -73,6 +55,18 @@ function addingAndRemovingProduct(product, isIncrease) {
     }
 
     document.getElementById(product + '-price').innerText = productPrice;
+
+    subTotal();
 };
 
-// Grand Total
+// Total
+
+function subTotal() {
+    let subPriceTotal = (phonePriceValue * document.getElementById('phone-count').value) + (casePriceValue * document.getElementById('case-count').value);
+    
+    document.getElementById('subTotal').innerText = subPriceTotal;
+
+    // No Tex
+    document.getElementById('grandTotal').innerText = subPriceTotal;
+
+}
